01a6ea7ed548b894e2076daaea7b92d0ecbf288f
alisiak03 <145369992+alisiak03@users.noreply.github.com>
2024-05-14 11:31:54 +0100

    navigational

---
+++ RoomNav.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alisia Kazimierek on 14/05/2024.
+//
+
+#include "RoomNav.h"
---
+++ RoomNav.h
@@ -0,0 +1,14 @@
+//
+// Created by Alisia Kazimierek on 14/05/2024.
+//
+
+#ifndef ZORK_ROOMNAV_H
+#define ZORK_ROOMNAV_H
+#include "Room.h"
+
+class RoomNav {
+
+};
+
+
+#endif //ZORK_ROOMNAV_H
